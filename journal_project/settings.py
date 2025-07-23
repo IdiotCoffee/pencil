3e5625941cebc1464d3815afe0b80ed1201cdd24
ishaan <ishaanmavinkurve16.com>
2025-07-23 21:31:24 +0530

    connecting to postgres

--- journal_project/settings.py
+++ journal_project/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
+
 
 # Load environment variables
 load_dotenv()
@@ -57,12 +59,12 @@ WSGI_APPLICATION = 'journal_project.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
